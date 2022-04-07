4d5385bb7a702ae17275177ecfc2e59838d213c5
TauTheLepton <matipalczuk@gmail.com>
2022-04-07 23:34:34 +0200

    added lab6 files
    label appears to work, but pitchwin does not fully work yet

--- lab6/label.m
+++ lab6/label.m
@@ -1,8 +1,44 @@
 function lbls = label(x, fs)
-%LABEL Summary of this function goes here
-%   Detailed explanation goes here
-win = 100;
-x = x - mean(x);
-x = x / max(x);
-end
 
+lbls = zeros(size(x));
+voiced_threshold = 0.4;
+speech_threshold = 0.06;
+
+win = 100;
+win = round(fs * 0.025);
+
+% x = x - mean(x);
+% x = x / max(x);
+
+% energia chwilowa
+ste = conv(x.^2, ones(1,win), 'same');
+ste = ste / max(ste);
+
+% wersja z pierwiastkiem
+str = sqrt(ste);
+
+speech = zeros(size(x));
+zcrs = zeros(size(x));
+for i = 1 : win : length(x)-win
+    if mean(str(i:i+win)) > speech_threshold
+        speech(i:i+win) = 1;
+    end
+    zcr = zerocrossrate(x(i:i+win));
+    zcrs(i:i+win) = zcr;
+    if zcr > voiced_threshold
+        lbls(i:i+win) = 1; % bezdzwieczne
+    else
+        lbls(i:i+win) = 2; % dzwieczne
+    end
+end
+lbls = lbls .* speech;
+
+% subplot(2, 1, 1);
+% hold on;
+% stairs(x);
+% stairs(lbls);
+% hold off;
+% subplot(2, 1, 2);
+% stairs(zcrs)
+
+end
--- lab6/pitchwin.m
+++ lab6/pitchwin.m
@@ -1,53 +1,34 @@
-[x, fs] = audioread("aeiouy.wav");
-% function f0 = pitchwin(x, fs, win)
-% x = x - mean(x);
-% x = x / max(x);
-
-% długość okna analizy: 200 ms
-win = round(fs * 0.1);
-
-% energia chwilowa
-ste = conv(x.^2, ones(1,win), 'same');
-ste = ste / max(ste);
-
-% wersja z pierwiastkiem
-str = sqrt(ste);
-
-speech = str > 0.05;
-% f0 = speech;
-
-% frekwencja bazowa
+function f0 = pitchwin(x, fs, win)
 
 f_max = 300;
 lag_min = fs / f_max;
 
 f0 = zeros(size(x));
-for i=1:win:length(x)-win
-
-    [c, lags] = xcorr(x(i:i+win), "normalized");
+for i=1 : win : length(x)-win
+    x_tmp = x(i:i+win);
+    [c, lags] = xcorr(x_tmp, "normalized");
     c = c(lags > lag_min);
     lags = lags(lags > lag_min);
     [pks, loc] = findpeaks(c, "MinPeakProminence", 0.2, "MinPeakWidth", 3);
-    
-    [peak, idx] = maxk(pks, 1);
-    peak;
-    lags(loc(idx));
-    if size(lags(loc(idx))) == [1 0]
-        pass=0;
-    else
+    [peak, idx] = max(pks);
+    if size(lags(loc(idx))) == [1 1]
         f0(i:i+win) = lags(loc(idx));
+%         disp("DOBRZE");
+%     else
+%         disp("ZLE");
     end
 end
-t = linspace(0, length(x)/fs, length(x));
-subplot(2, 1, 1);
+
+% t = linspace(0, length(x)/fs, length(x));
+% [pks, loc] = findpeaks(c, "MinPeakProminence", 0.2, "MinPeakWidth", 3);
+% subplot(2, 1, 1);
 % plot(lags, c);
-plot(t, x);
-subplot(2, 1, 2);
-plot(t, f0);
-ylim([0 200]);
-% hold on;
-% plot(lags(loc),s pks, "x");
-% xlim([0 0.5])
+% plot(t, x);
+% % hold on;
+% % plot(lags(loc), pks, "x");
+% 
+% subplot(2, 1, 2);
+% plot(t, f0');
+% ylim([0 200]);
 
-% end
-
+end
